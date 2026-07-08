47d5d3e87e885d475a9987fea249fe3e2798441b
marat30pipo-blip <marat30pipo@gmail.com>
2026-07-08 04:56:59 +0300

    Реализован метод findAllByProfileDescription(String profileDescription) для базы данных

---
+++ src/main/java/task5/shop/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.account (name, email, password, profiledescription) VALUES ('Marat', 'Marat@mail.ru', '12345', 'The best');
+INSERT INTO public.account (name, email, password, profiledescription) VALUES ('Kostya', 'Kostya@mail.ru', '8548', 'Good');
